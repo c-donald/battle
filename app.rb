efb81ca23ca8bd7c82e1079f400ca567188521fb
Veronica Lee <veronicavlee@icloud.com>
2020-09-15 14:57:47 +0100

    Add h1 name string to cat

--- app.rb
+++ app.rb
@@ -2,23 +2,23 @@ require 'sinatra'
 
 
 get '/' do
-    "HELLO WORLD"
+  "HELLO WORLD"
 end
 
 get '/secret' do
-    "This is a secret!"
+  "This is a secret!"
 end
 
 get '/othersecret' do
-    "This is secret as well"
+  "This is secret as well"
 end
 
-get '/anotherfile' do 
-    "add another file"
+get '/anotherfile' do
+  "add another file"
 end
 
-get '/cat' do 
- '<img src="http://bit.ly/1eze8aE" style="border: dashed red;">'
+get '/cat' do
+  erb(:index)
 end
 
 set :session_secret, 'super secret'
