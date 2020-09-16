cbdadf0453cecdee997837d0a77470cc15e57c88
samcolson4 <samcolson4@gmail.com>
2020-09-16 15:33:19 +0100

    Added player one & two name forms && feature test

--- app.rb
+++ app.rb
@@ -6,4 +6,14 @@ class Battle < Sinatra::Base
     'Testing infrastructure working!'
    end 
 
+   get '/names' do
+      erb :index
+   end
+
+   post '/names' do
+      @player_one = params[:player_one]
+      @player_two = params[:player_two]
+      erb :play
+   end
+
 end
--- spec/features/testing_spec.rb
+++ spec/features/testing_spec.rb
@@ -4,3 +4,13 @@ feature 'Testing infrastructure' do
     expect(page).to have_content "Testing infrastructure working!"
   end 
 end
+
+feature 'players submit names in a form' do
+  scenario 'players enter names in a form and then see their names on screen' do
+    visit '/names'
+    fill_in('player_one', with: 'Sam')
+    fill_in('player_two', with: 'Courtenay')
+    click_on('Submit')
+    expect(page).to have_content "Sam vs. Courtenay"
+  end
+end
