a05c8babb9b90d8fe505808e36ff004a63ccc09d
samcolson4 <samcolson4@gmail.com>
2020-09-17 14:59:12 +0100

    Refactored to use constants in test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,7 +9,7 @@ describe Game do
   describe '#attack' do
     it 'removes 10 HP from a player' do
       defender = Player.new("T")
-      expect { subject.attack(defender) }.to change { defender.hp }.from(100).to(90)
+      expect { subject.attack(defender) }.to change { defender.hp }.from(Player::HP).to(Player::HP - Player::ATTACK_VALUE)
     end
   end
 
